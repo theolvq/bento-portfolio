24e9d11a0a06b7c6422d7d9321918e4a0a82ef87
Théo Lévêque <theo.leveque@gmail.com>
2024-12-06 15:39:04 -0800

    add wrapper card

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	type Props = SvelteHTMLElements['div'] & {
+		direction: 'top-left' | 'right' | 'bottom';
+		delay: number;
+		class?: string;
+	};
+
+	import { flyAndFade } from '$lib/transitions';
+	const { children, direction, delay, class: className }: Props = $props();
+</script>
+
+<div
+	in:flyAndFade={{ direction, delay }}
+	class={`flex flex-col justify-between rounded-3xl bg-white/10 p-8 text-white backdrop-blur-sm ${className}`}
+>
+	{@render children?.()}
+</div>
